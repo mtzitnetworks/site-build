be00e561806473e85f5507c051cea36cdef59c28
Kyndrea Martinez <kyndream@gmail.com>
2026-04-29 22:37:30 -0500

    metadata

---
+++ src/_data/metadata.js
@@ -0,0 +1,10 @@
+export default {
+    email: {
+        string: "contact@mywebsite.com",
+        link: "mailto:contact@mywebsite.com"
+    },
+    tel: {
+        number: "(555) 555-5555",
+        link: "tel:555-555-5555",
+    } 
+}
